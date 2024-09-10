3d8bc7129c3e138e8b74a8de0b0e3d545f1cee3f
PutriMaharani06 <12350123000@students.uin-suska.ac.id>
2024-09-10 18:07:56 +0700

    Add files via upload

---
+++ BiodataDiri.java
@@ -0,0 +1,13 @@
+public class BiodataDiri {
+    public static void main(String[] args) {
+        String nama = "Putri Maharani";
+        String NIM = "12350123000";
+        String hobi = "Menonton dan Memasak";
+        System.out.println("    Biodata Diri    ");
+        System.out.println("==========================");
+        System.out.println("Nama    : " + nama);
+        System.out.println("NIM     : " + NIM);
+        System.out.println("hobi    : " + hobi);
+        System.out.println("==========================");
+    }
+}
